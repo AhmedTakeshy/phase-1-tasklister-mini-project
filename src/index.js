4868f4d3dfd0a3e7b44de0334d9567b89b26d502
Ahmed Abdelsamie <Ahmedtakeshy7@gmail.com>
2023-05-12 19:12:16 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const task = document.createElement("li");
+    task.innerText = e.target[0].value;
+    taskList.appendChild(task);
+    e.target.reset();
+  });
 });
